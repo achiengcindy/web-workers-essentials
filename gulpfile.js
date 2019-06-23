5253023e79decc257a409541711b5800ed384ef1
achieng cindy <achiengcindy36@gmail.com>
2019-06-23 16:06:03 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+// const gulp = require("gulp");
+// const browserSync = require('browser-sync').create();
+
+// // Static Server
+
+// // we write functions now in gulp 4
+
+// // convert images we
+// function convertImage() {
+//   return pipeline(
+//     gulp.src("./src/static/img/**/*"),
+//     webp(),
+//     gulp.dest("./src/static/img/")
+//   );
+// };
+
+// exports.convertImage = convertImage;
